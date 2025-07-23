d7bd27869e0dcfb2a780280a05bd939ad6f06686
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-07-23 09:58:23 +0530

    Update cls_assignment.sv

--- Classes/Class_assignment/cls_assignment.sv
+++ Classes/Class_assignment/cls_assignment.sv
@@ -1,21 +1,37 @@
- class packet;
-   string a;
-   int b;
-   function new();
-     a="apple";
-     b=5;
-   endfunction
-   function display();
-     $display("a=%0s,b=%0d",a,b);
-   endfunction
- endclass
+// Code your design here
+class packet;
+  string c;
+  int d;
+  
+  function new();
+    c="APPLE";
+    d=4;
+  endfunction:new
+  
+  function void display();
+    $display("c=%0s, d=%0d",c,d);
+  endfunction:display
+
+endclass:packet
+
 module assignment;
-  initial begin
   packet p1;
   packet p2;
-  
+
+  initial begin
     p1=new();
-    p1.display();
-  end
-endmodule
     
+    p1.display();
+    p2=p1;
+    
+    p2.display();
+    p2.c="GURU";
+    p2.d=8;
+    
+    p1.display();
+   
+    p2.display();
+  end
+
+endmodule
+   
