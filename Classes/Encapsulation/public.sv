408e7df683fedad0ccb9d7b34993dbd05e93d654
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-10-07 15:18:18 +0530

    Create public.sv

---
+++ Classes/Encapsulation/public.sv
@@ -0,0 +1,33 @@
+class encapsulation;
+  int a;
+  int b;
+  int c;
+  function new();
+    a=2;
+    b=4;
+    c=5;
+  endfunction
+  function void display();
+        $display("......a=%0d,b=%0d,c=%0d",a,b,c);
+  endfunction
+endclass
+class encap extends encapsulation;
+   function void display();
+        $display("......a=%0d,b=%0d,c=%0d",a,b,c);
+  endfunction
+endclass
+  module en;
+    encapsulation e0;
+    encap e1;
+    initial begin
+      e0=new();
+      e1=new();
+      e0.display();
+      e1.display();
+    end
+  endmodule
+
+
+output:
+# KERNEL: ......a=2,b=4,c=5
+# KERNEL: ......a=2,b=4,c=5
