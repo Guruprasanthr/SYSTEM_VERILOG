dd58ac9b1706056eae273c4b130708957e358168
Guruprasanthr <guruprasanthrsh@gmail.com>
2025-10-09 11:42:24 +0530

    Create fork_join.sv

---
+++ Threads/fork_join.sv
@@ -0,0 +1,24 @@
+module frk;
+  initial begin
+    $display("...................");
+    fork
+      begin
+        #5 $display("process 1");
+      #10 $display("process 2");
+      end
+      begin
+        $display("process 3");
+        #20 $display("process 4");
+      end
+    join
+    $display("process end");
+  end
+endmodule
+
+output:
+# KERNEL: ...................
+# KERNEL: process 3
+# KERNEL: process 1
+# KERNEL: process 2
+# KERNEL: process 4
+# KERNEL: process end
